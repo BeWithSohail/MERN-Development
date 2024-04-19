f941ca27b6d5d072b556e9bb046bc9ea62fda407
BeWithSohail <sohailalam6027@gmail.com>
2024-04-19 11:01:43 +0530

    removed unnecessary file

--- EXPRESS JS/userRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const registerUser = require("./userController");
-router.route("/userdata").get(registerUser);
-router.route("/register").post(registerUser);
-router.route("/product/3").put(registerUser);
-router.route("/product").delete(registerUser);
-
-module.exports = router;
